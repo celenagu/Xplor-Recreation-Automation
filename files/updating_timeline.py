d41a6829faf99583837567fa1486893a8a2c76b1
Grewal <Aggam.Grewal@brampton.ca>
2025-01-28 11:31:58 -0500

    adding new file

---
+++ files/updating_timeline.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+import time, csv, json
+
+# Constants
+TEST_ENVIRONMENT = False
+FILE_NAME = "updating_timeline.csv"
+# login based on the environment
+if not TEST_ENVIRONMENT:
+	LOGIN_URL = "https://cityofbrampton.perfectmind.com/Menu/MemberRegistration/MemberSignIn"
+	USERNAME = "[YOUR LOGIN USERNAME]" # replace
+	print("!===WARNING: LIVE MODE===!")
+else:
+	LOGIN_URL = "https://cityofbramptontest.perfectmind.com/Contacts/MemberRegistration/MemberSignIn"
+	USERNAME = "Train8"
+	print("===TEST MODE===")
+
+# Webdriver Initialization
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+
+# Functions
+def log_save(file, s):
+	# writes string to file and prints to console
+	file.write(s + "\n") # reformat as needed
+	print(s)
+
+def click_js(driver, elem):
+	# click element using JavaScript
+	driver.execute_script("arguments[0].click();", elem)
+
+# Open website and login
+driver.get(LOGIN_URL)
+driver.find_element("id", "textBoxUsername").send_keys(USERNAME)
+action = ActionChains(driver)
+
