3ea1c9b9147d7318dde0ad38c0b3f446badb73d4
Celena <sw.celenagu@gmail.com>
2025-04-09 15:33:26 -0400

    update

--- files/validate_residency/validate_residency.py
+++ files/validate_residency/validate_residency.py
@@ -11,31 +11,22 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 import time, csv, json
 
 from webdriver_manager.chrome import ChromeDriverManager
- 
-read_file_name = "barcodes.csv"
- 
-#reading the file
-file_names = pd.read_csv(read_file_name, dtype={"EventID": str})
- 
-#getting the barcodes from the specified column
-column_name = "EventID"          
-all_barcodes = file_names[column_name].dropna().tolist()  
- 
+
  
 # Constants
-TEST_ENVIRONMENT = False
+TEST_ENVIRONMENT = True
 FILE_NAME = "update_log.csv"
 new_date = "2060-01-1"
  
 # login based on the environment
 if not TEST_ENVIRONMENT:
-    LOGIN_URL = "https://cityofbrampton.perfectmind.com/Menu/MemberRegistration/MemberSignIn"
-    USERNAME = "[YOUR LOGIN USERNAME]" # replace
-    print("!===WARNING: LIVE MODE===!")
+	LOGIN_URL = "https://cityofbrampton.perfectmind.com/Menu/MemberRegistration/MemberSignIn"
+	USERNAME = "[YOUR LOGIN USERNAME]" # replace
+	print("!===WARNING: LIVE MODE===!")
 else:
-    LOGIN_URL = "https://cityofbramptontest.perfectmind.com/Contacts/MemberRegistration/MemberSignIn"
-    USERNAME = "Train8"
-    print("===TEST MODE===")
+	LOGIN_URL = "https://cityofbramptontest.perfectmind.com/Contacts/MemberRegistration/MemberSignIn"
+	USERNAME = "Train8"
+	print("===TEST MODE===")
  
 # Webdriver Initialization
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -43,13 +34,13 @@ driver.implicitly_wait(10)
  
 # ----------------------------------- FUNCTIONS ------------------------------------------------------------------------
 def log_save(file, s):
-    # writes string to file and prints to console
-    file.write(s + "\n") # reformat as needed
-    print(s)
+	# writes string to file and prints to console
+	file.write(s + "\n") # reformat as needed
+	print(s)
  
 def click_js(driver, elem):
-    # click element using JavaScript
-    driver.execute_script("arguments[0].click();", elem)
+	# click element using JavaScript
+	driver.execute_script("arguments[0].click();", elem)
 
 # -----------------------------------------------------------------------------------------------------------------------------------
  
@@ -60,9 +51,75 @@ action = ActionChains(driver)
  
  
 #---------------------------------OVERALL LOOP------------------------------------------------
+# Note: This loop is only to process a single display page in XPLOR (max 1000 items)
+# To process views with more than 1000 items, the program will have to be monitored every 1000 items (to manually navigate to next page)
+# (May change later)
+
 while True:
 	user_input = input("Press ENTER to start/continue or type 'exit' to end: ").strip().lower()
  
 	if user_input == "exit":
 		break
+	  
+	# User defined-range for facility indices
+	start_idx = int(input("Enter the START value: "))
+	end_idx = int(input("Enter the END value: "))
+
+	curr_idx = start_idx
+	  
+	# fetch the list of accounts to be referenced later (avoiding back-navigating)
+	account_links = driver.find_elements("xpath", "//a[contains(@class, 'recordDetailIcon')]")
+	account_urls = [link.get_attribute("href") for link in account_links]
  
+	with open(FILE_NAME, "w", newline="", encoding="utf-8") as file:
+		# for populating csv file
+		writer = csv.writer(file)
+		writer.writerow(["Client Name", "Account Name", "Account Type", "Account Category", "Account ID", "Client Contact ID", "Validated?"])
+			
+		# Open up new window
+		source_window = driver.current_window_handle
+		driver.execute_script("window.open('');")
+
+		all_windows = driver.window_handles
+		driver.switch_to.window(all_windows[-1])
+		
+		for curr_idx, url in enumerate(account_urls[start_idx:end_idx+1], start = start_idx):
+			print()
+			print(f"Processing current facility index: {curr_idx}")
+
+			account_data = {
+				"Account Name": "",         
+				"Account Type": "",
+				"Account Category": "",
+				"Account ID": ""
+			}
+
+			# Attempt click account
+			try: 
+				driver.get(url)	# open next account in new tab
+				WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "box-tab-caption-active")))
+
+				# fetch data needed to populate CSV log (account data)
+				account_data["Account Name"] = driver.find_element(By.XPATH, "//tr[contains(@class, 'Name-wrapper')]//div[contains(@class, 'field-web-control')]").text
+				account_data["Account Type"] = driver.find_element(By.XPATH, "//tr[contains(@class, 'AccountType-wrapper')]//div[contains(@class, 'field-web-control')]").text
+				account_data["Account Category"] = driver.find_element(By.XPATH, "//tr[contains(@class, 'AccountCategory-wrapper')]//div[contains(@class, 'field-web-control')]").text
+				account_data["Account ID"] = driver.find_element(By.XPATH, "//tr[contains(@class, 'RecordName-wrapper')]//div[contains(@class, 'field-web-control')]").text
+				
+				# get clients associated with account
+				clients = driver.find_elements("xpath", "//li[contains(@class, 'family-member') and not (@id='family-member-template')]//h3[contains(@class, 'family-member-name')]")
+
+
+				for client in clients:
+					print(client)
+					click_js(driver, client)
+					WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "box-tab-caption-active")))
+
+					input("Paused. Press Enter to continue...")
+
+					# Handle updating residency
+
+					back = driver.find_element(By.XPATH, "//a[contains(@class, 'back-button-link')]")
+					click_js(driver, back)
+
+			except Exception as e:
+				print(f"Failed to update account index {curr_idx} due to error:\n{e}")
