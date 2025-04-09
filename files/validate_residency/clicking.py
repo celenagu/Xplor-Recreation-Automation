f0e13760a03ca5ebcd06f80b1c9a64e4e1e006c6
Grewal <Aggam.Grewal@brampton.ca>
2025-04-09 13:47:50 -0400

    aggam's code

---
+++ files/validate_residency/clicking.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+ 
+import time, csv, json
+
+from webdriver_manager.chrome import ChromeDriverManager
+
