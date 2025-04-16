af7ac19abfd8e4a75fcb3bda8b470ac55a3c14aa
Celena <sw.celenagu@gmail.com>
2025-04-16 09:57:49 -0400

    update

--- files/validate_residency/validate_residency.py
+++ files/validate_residency/validate_residency.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
@@ -178,10 +179,16 @@ while True:
                         date_button.send_keys(new_date)
                         time.sleep(1)
 
+                        # Handle owner validation error
+                        owner_dropdown_element = driver.find_element("xpath", "//select[contains(@class, 'pmuserpicker')]")
+                        owner_dropdown = Select(owner_dropdown_element)
+                        owner_dropdown.select_by_visible_text("PM Support")
+
                         #find the save button
                         save = driver.find_element(By.ID, "submitButton")
                         click_js(driver, save)
-                        time.sleep(4)
+                        time.sleep(3)
+
 
                         print("Saved")
 
